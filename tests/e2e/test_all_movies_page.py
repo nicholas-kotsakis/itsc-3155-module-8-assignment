2adafe6e92026fd0c746ba6b06eba48f8ff7bcdd
nicholas-kotsakis <nkotsakis13@gmail.com>
2022-03-13 19:18:46 -0400

    e2e and unit tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 1
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_list_all_movies(test_app):
+    movie_repository_singleton.create_movie('Test Movie Title','Test Director',5)
+    response = test_app.get('/movies')
+    assert b"Test Movie Title" in response.data
+    test_string = str(response.data)
+    assert test_string.count('<i class=\"fas fa-star\" style=\"color: gold;\"></i>') == 5
+    assert b"Test Director" in response.data
+    movie_repository_singleton.get_all_movies().clear()
+
+
+
