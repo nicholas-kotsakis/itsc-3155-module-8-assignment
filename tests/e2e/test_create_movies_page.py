45200963651a5e7ac72803a363e3aef3a940b7b6
James Sprague <jsmans89@gmail.com>
2022-03-14 14:05:14 -0400

    Finished test_create_movie test, still working on test_create_movies_page test

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,23 +1,24 @@
 # TODO: Feature 2
 from flask import request
+from app import app
 from src.repositories.movie_repository import movie_repository_singleton
 
-def test_create_movie():
+def test_create_movie(test_app):
     title1 = "Awesome Movie Title"
     director1 = "Person Guy"
     rating1 = 3
     title2 = "Not As Awesome Movie Title"
     director2 = "Guy Person"
     rating2 = 1
-    send_request = request.post("/movies",title=title1,director=director1,rating=rating1)
-    saved_movies = movie_repository_singleton.get_all_movies()
-    assert saved_movies[0].title == title1
-    assert saved_movies[0].director == director1
-    assert saved_movies[0].rating == rating1
-    assert len(saved_movies) == 1
-    send_request = request.post("/movies",title=title2,director=director2,rating=rating2)
-    saved_movies = movie_repository_singleton.get_all_movies()
-    assert saved_movies[1].title == title2
-    assert saved_movies[1].director == director2
-    assert saved_movies[1].rating == rating2
-    assert len(saved_movies) == 2
+    # send_request = app.post('/movies',title=title1,director=director1,rating=rating1)
+    # saved_movies = movie_repository_singleton.get_all_movies()
+    # assert len(saved_movies) == 1
+    # assert saved_movies[0].title == title1
+    # assert saved_movies[0].director == director1
+    # assert saved_movies[0].rating == rating1
+    # send_request = request.post("/movies",title=title2,director=director2,rating=rating2)
+    # saved_movies = movie_repository_singleton.get_all_movies()
+    # assert len(saved_movies) == 2
+    # assert saved_movies[1].title == title2
+    # assert saved_movies[1].director == director2
+    # assert saved_movies[1].rating == rating2
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,23 +1,20 @@
 # TODO: Feature 2
 from flask import request
+from app import app
 from src.repositories.movie_repository import movie_repository_singleton
 
 def test_create_movie():
-    title1 = "Awesome Movie Title"
-    director1 = "Person Guy"
-    rating1 = 3
-    title2 = "Not As Awesome Movie Title"
-    director2 = "Guy Person"
-    rating2 = 1
-    send_request = request.post("/movies",title=title1,director=director1,rating=rating1)
+    title1, director1, rating1 = "Awesome Movie Title", "Person Guy", 3
+    title2, director2, rating2 = "Not As Awesome Movie Title", "Guy Person", 1
+    movie_repository_singleton.create_movie(title1,director1,rating1)
     saved_movies = movie_repository_singleton.get_all_movies()
+    assert len(saved_movies) == 1
     assert saved_movies[0].title == title1
     assert saved_movies[0].director == director1
     assert saved_movies[0].rating == rating1
-    assert len(saved_movies) == 1
-    send_request = request.post("/movies",title=title2,director=director2,rating=rating2)
+    movie_repository_singleton.create_movie(title2,director2,rating2)
     saved_movies = movie_repository_singleton.get_all_movies()
+    assert len(saved_movies) == 2
     assert saved_movies[1].title == title2
     assert saved_movies[1].director == director2
     assert saved_movies[1].rating == rating2
-    assert len(saved_movies) == 2
