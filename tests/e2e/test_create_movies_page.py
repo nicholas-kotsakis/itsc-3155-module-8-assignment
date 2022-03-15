465976b8d4628aa0eb1402d4fcac4a45ab089fd3
James Sprague <jsmans89@gmail.com>
2022-03-15 16:28:15 -0400

    Finished pytests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,24 +1,24 @@
 # TODO: Feature 2
-from flask import request
-from app import app
 from src.repositories.movie_repository import movie_repository_singleton
 
-def test_create_movie(test_app):
-    title1 = "Awesome Movie Title"
-    director1 = "Person Guy"
-    rating1 = 3
-    title2 = "Not As Awesome Movie Title"
-    director2 = "Guy Person"
-    rating2 = 1
-    # send_request = app.post('/movies',title=title1,director=director1,rating=rating1)
-    # saved_movies = movie_repository_singleton.get_all_movies()
-    # assert len(saved_movies) == 1
-    # assert saved_movies[0].title == title1
-    # assert saved_movies[0].director == director1
-    # assert saved_movies[0].rating == rating1
-    # send_request = request.post("/movies",title=title2,director=director2,rating=rating2)
-    # saved_movies = movie_repository_singleton.get_all_movies()
-    # assert len(saved_movies) == 2
-    # assert saved_movies[1].title == title2
-    # assert saved_movies[1].director == director2
-    # assert saved_movies[1].rating == rating2
+def test_create_movies_page(test_app):
+    # Assert webpage contents
+    response = test_app.get('/movies/new')
+    assert b'<h1 class="mb-5">Create Movie Rating</h1>' in response.data
+    
+    data = {
+        'title':'Not As Awesome Movie Title',
+        'director':'Guy Person',
+        'rating': 2
+    }
+    # Test POST response 
+    # Get previous length to make sure the length only increased by 1
+    prev_len = len(movie_repository_singleton.get_all_movies())
+    response = test_app.post('/movies',data=data)
+    saved_movies = movie_repository_singleton.get_all_movies()
+    assert len(saved_movies) == prev_len + 1
+    # Find index of new movie at end of array
+    i = len(saved_movies) - 1
+    assert saved_movies[i].title == data['title']
+    assert saved_movies[i].director == data['director']
+    assert saved_movies[i].rating == data['rating']
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,20 +1,21 @@
 # TODO: Feature 2
-from flask import request
-from app import app
+from turtle import title
 from src.repositories.movie_repository import movie_repository_singleton
 
 def test_create_movie():
-    title1, director1, rating1 = "Awesome Movie Title", "Person Guy", 3
-    title2, director2, rating2 = "Not As Awesome Movie Title", "Guy Person", 1
-    movie_repository_singleton.create_movie(title1,director1,rating1)
+    data = {
+        'title':'Awesome Movie Title',
+        'director':'Person Guy',
+        'rating': 5
+    }
+    # Get previous length to make sure the length only increased by 1
+    prev_len = len(movie_repository_singleton.get_all_movies())
+    # Test create_movie function
+    movie_repository_singleton.create_movie(data['title'],data['director'],data['rating'])
     saved_movies = movie_repository_singleton.get_all_movies()
-    assert len(saved_movies) == 1
-    assert saved_movies[0].title == title1
-    assert saved_movies[0].director == director1
-    assert saved_movies[0].rating == rating1
-    movie_repository_singleton.create_movie(title2,director2,rating2)
-    saved_movies = movie_repository_singleton.get_all_movies()
-    assert len(saved_movies) == 2
-    assert saved_movies[1].title == title2
-    assert saved_movies[1].director == director2
-    assert saved_movies[1].rating == rating2
+    assert len(saved_movies) == prev_len + 1
+    # Find index of new movie at end of array
+    i = len(saved_movies) - 1
+    assert saved_movies[i].title == data['title']
+    assert saved_movies[i].director == data['director']
+    assert saved_movies[i].rating == data['rating']
