13fc51721fe0b8ac33e7b13f785c8e4e4223daab
Will Paul <wspeigh3@uncc.edu>
2022-03-15 21:33:26 -0400

    adding push status

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,15 @@
+
 # TODO: Feature 3
+
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_movie_by_title():
+    
+    movie = movie_repository_singleton.create_movie('Star Wars', 'George Lucas', 5)
+    #make sure movie tile is the same 
+    assert movie.title == 'Star Wars'
+    movie2 = movie_repository_singleton.create_movie('', 'George Lucas', 5)
+    
+    assert movie2.title == None
+    movie_repository_singleton.get_all_movies().clear()
+
